d294b2c2faef080c8a63bec743c9b406b6202983
SGarabiles-9R <garabiles.sg@obmontessori.edu.ph>
2025-03-10 20:31:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 
+
+                    function calculateFare() {
+                        let startStation = parseInt(document.getElementById("startStation").value);
+                        let endStation = parseInt(document.getElementById("endStation").value);
+                        let passengerType = document.getElementById("passengerType").value;
+                        
+                        let distance = Math.abs(endStation - startStation);
+                        let baseFare = 13 + (distance * 2);
+                        
+                        if (passengerType === "student" || passengerType === "senior") {
+                            baseFare *= 0.2; 
+                        }
+                        
+                        document.getElementById("fare-amount").innerText = baseFare.toFixed(2);
+                    }
